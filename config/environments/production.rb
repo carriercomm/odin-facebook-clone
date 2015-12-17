25820ee6cf1ec2532e8ee52c2d2952dee102e133
Robert Feie <rfeie@hotmail.com>
2015-12-16 20:05:29 -0600

    added sendgrid customizations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,17 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
+  host = 'secure-river-3040.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host}
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttles_auto => true
+  }
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
